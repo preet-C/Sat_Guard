f15b3bd81f60cc8592f93f4b138db0fdba105969
Preetham C G <zorororonova256@gmail.com>
2026-03-19 20:54:19 +0530

    added gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      SatGuard 🚀
-    </h1>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-purple-600">
+        SatGuard 🚀
+      </h1>
+    </div>
   )
 }
 
